121779f05c6be72ca4a7842df240b81b9859fc2b
La pyae <demon12442@gmail.com>
2023-08-02 19:54:03 +0630

    google analytics

---
+++ assets/analytics.js
@@ -0,0 +1,503 @@
+///////////////////////////////////////////////
+//        Volume btn click listender         //
+/////////////////////////////////////////////*/
+document.getElementById("myButton").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Volume button Click",
+    event_label: "Volume Button",
+  });
+});
+///////  End of Volume btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Achievement Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("achievementBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Achievement button Click",
+      event_label: "Achievement Button",
+    });
+  });
+////  End of Achievement Btn click listender  /////
+
+///////////////////////////////////////////////
+//      Achievement back Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("achievementbackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Achievement back button Click",
+      event_label: "Achievement back Button",
+    });
+  });
+////  End of Achievement Btn click listender  /////
+
+///////////////////////////////////////////////
+//      Setting Btn click listender      //
+/////////////////////////////////////////////*/
+document.getElementById("settingBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Setting button Click",
+    event_label: "Setting Button",
+  });
+});
+///////  End of Setting Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Setting back Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("settingBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Setting back button Click",
+      event_label: "Setting back Button",
+    });
+  });
+///////  End of Setting Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Music Btn click listender      //
+/////////////////////////////////////////////*/
+document.getElementById("musicBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Music on off button Click",
+    event_label: "Music on of switch button",
+  });
+});
+///////  End of Music Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Betting time reduce Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("bettingTimeReduce")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Betting time reduce btn click",
+      event_label: "Betting time reduce button",
+    });
+  });
+///////  End of Betting time reduce Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Betting time reduce Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("bettingTimeIncrease")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Betting time increase btn click",
+      event_label: "Betting time increase button",
+    });
+  });
+///////  End of Betting time reduce Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Privacy Btn click listender      //
+/////////////////////////////////////////////*/
+document.getElementById("privacyBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Privacy btn click",
+    event_label: "Privacy button",
+  });
+});
+///////  End of Betting time increase Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Privacy Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("privacyBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Privacy back btn click",
+      event_label: "Privacy back button",
+    });
+  });
+///////  End of privacyBackBtn Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      userAgreement Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("userAgreementBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "userAgreement btn click",
+      event_label: "userAgreement button",
+    });
+  });
+///////  End of userAgreement Btn Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      userAgreement Back Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("userAgreementBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "userAgreement back btn click",
+      event_label: "userAgreement back button",
+    });
+  });
+///////  End of userAgreement Back Btn Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      About betting slots Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("aboutBettingSlotsBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "About betting slots btn click",
+      event_label: "About betting slots button",
+    });
+  });
+///////  End of About betting slots Btn Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      About betting slots back Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("aboutBettingSlotsBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "About betting slots back btn click",
+      event_label: "About betting slots back button",
+    });
+  });
+///////  End of About betting slots back Btn Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Sign in Btn click listender      //
+/////////////////////////////////////////////*/
+document.getElementById("signInBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Sign in btn click",
+    event_label: "Sign in button",
+  });
+});
+///////  Sign out Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Sign out Btn click listender      //
+/////////////////////////////////////////////*/
+document.getElementById("signOutBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Sign out btn click",
+    event_label: "Sign out button",
+  });
+});
+///////  Sign out Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Profile Btn click listender      //
+/////////////////////////////////////////////*/
+document.getElementById("profileBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Profile btn click",
+    event_label: "Profile button",
+  });
+});
+///////  Profile Btn click listender  ////////
+
+///////////////////////////////////////////////
+//      Profile Btn click listender      //
+/////////////////////////////////////////////*/
+document
+  .getElementById("profileBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Profile back btn click",
+      event_label: "Profile back button",
+    });
+  });
+///////  Profile Btn click listender  ////////
+
+///////////////////////////////////////////////
+//           Play Btn click listender        //
+/////////////////////////////////////////////*/
+document.getElementById("playBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Play btn click",
+    event_label: "Play button",
+  });
+});
+///////     Play click listender      ////////
+
+///////////////////////////////////////////////
+//           Betting items click listender        //
+/////////////////////////////////////////////*/
+document.getElementById("dogBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Dog btn click",
+    event_label: "Dog button",
+  });
+});
+
+document.getElementById("monkey").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Monkey btn click",
+    event_label: "Monkey button",
+  });
+});
+
+document.getElementById("sheep").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Sheep btn click",
+    event_label: "Sheep button",
+  });
+});
+
+document.getElementById("elephant").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Elephant btn click",
+    event_label: "Elephant button",
+  });
+});
+
+document.getElementById("seahorse").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Seahorse btn click",
+    event_label: "Seahorse button",
+  });
+});
+
+document.getElementById("jellyfish").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Jellyfish btn click",
+    event_label: "Jellyfish button",
+  });
+});
+
+document.getElementById("dolphin").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Dolphin btn click",
+    event_label: "Dolphin button",
+  });
+});
+
+document.getElementById("shark").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Shark btn click",
+    event_label: "Shark button",
+  });
+});
+
+document.getElementById("bird").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Bird btn click",
+    event_label: "Bird button",
+  });
+});
+
+document.getElementById("whale").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Whale btn click",
+    event_label: "Whale button",
+  });
+});
+
+document.getElementById("sea").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Sea btn click",
+    event_label: "Sea button",
+  });
+});
+
+document.getElementById("land").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "land btn click",
+    event_label: "land button",
+  });
+});
+///////     Betting items click listender      ////////
+
+///////////////////////////////////////////////
+//           Game start Btn listender           //
+/////////////////////////////////////////////*/
+
+document.getElementById("gameStartBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Game start btn click",
+    event_label: "Game start button",
+  });
+});
+
+///////  End of Game start Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Game restart Btn listender           //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("gameRestartBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Game restart btn click",
+      event_label: "Game restart button",
+    });
+  });
+
+///////  End of Game restart Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Get more coin Btn listender           //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("getMoreCoinBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Get more coins btn click",
+      event_label: "Get more coins button",
+    });
+  });
+
+///////  End of Get more coin listender  ////////
+
+///////////////////////////////////////////////
+//           Get more coin Back Btn listender           //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("getMoreCoinBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Get more coins back btn click",
+      event_label: "Get more coins back button",
+    });
+  });
+
+///////  End of Get more coin back listender  ////////
+
+///////////////////////////////////////////////
+//           Game quit Btn listender           //
+/////////////////////////////////////////////*/
+
+document.getElementById("gamQuitBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Game quit btn click",
+    event_label: "Game quit button",
+  });
+});
+
+///////  End of Game quit Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Luck Spin Btn listender         //
+/////////////////////////////////////////////*/
+
+document.getElementById("luckySpinBtn").addEventListener("click", function () {
+  // console.log("hello");
+  gtag("event", "click", {
+    event_category: "Lucky spin btn click",
+    event_label: "lucky spin button",
+  });
+});
+
+///////  Luck Spin Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Luck Spin start Btn listender         //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("luckSpinStartBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Lucky spin start btn click",
+      event_label: "lucky spin start button",
+    });
+  });
+
+///////  Luck Spin start Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Luck Spin start Btn listender         //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("luckSpinBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Lucky spin back btn click",
+      event_label: "lucky spin back button",
+    });
+  });
+
+///////  Luck Spin back Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Daily reward spin history Btn listender         //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("dailyRewardSpinHistory")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Daily reward spin history btn click",
+      event_label: "Daily reward spin history button",
+    });
+  });
+
+///////  Daily reward spin history Btn listender  ////////
+
+///////////////////////////////////////////////
+//           Daily reward spin history Btn listender         //
+/////////////////////////////////////////////*/
+
+document
+  .getElementById("dailyRewardSpinHistoryBackBtn")
+  .addEventListener("click", function () {
+    // console.log("hello");
+    gtag("event", "click", {
+      event_category: "Daily reward spin history back btn click",
+      event_label: "Daily reward spin history back button",
+    });
+  });
+
+///////  Daily reward spin history Btn listender  ////////
